92843feedf1afc8a0ee186005b7ddec5b20bc9f8
PangBoy <zehao.xu163@gmail.com>
2023-01-31 14:22:39 +0800

    switch to dev

--- diy-part1.sh
+++ diy-part1.sh
@@ -20,4 +20,4 @@
 echo 'src-git netspeedtest https://github.com/sirpdboy/netspeedtest' >>feeds.conf.default
 echo 'src-git Istore  https://github.com/linkease/istore' >>feeds.conf.default
 echo 'src-git AdGuardHome  https://github.com/panther706/luci-app-adguardhome' >>feeds.conf.default
-echo 'src-git Openclash  https://github.com/vernesong/OpenClash;master' >>feeds.conf.default
+echo 'src-git Openclash  https://github.com/vernesong/OpenClash;dev' >>feeds.conf.default
